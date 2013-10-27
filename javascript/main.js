231b4fc6e6ad40ba8686362d9c9f31621a2ca5dd
Amit Kapadia <amit@zooniverse.org>
2013-10-27 14:36:48 +0000

    initial tutorial

---
+++ javascript/main.js
@@ -0,0 +1,25 @@
+(function() {
+  
+  // Set initial variables
+  var el = null;
+  var xAxisEl = null;
+  var yAxisEl = null;
+  var zAxisEl = null;
+  var data = null;
+  var r = null
+  
+  var dataPath = "data/dr7qso.json.gz";
+  
+  // Callback function for when an axis is selected. This function formats and
+  // plots the data.
+  function plot() {
+    
+  }
+  
+  // Run this function when the DOM is ready.
+  function DOMReady() {
+  }
+  
+  window.addEventListener('DOMContentLoaded', DOMReady, false);
+
+})()
